2719a521666e1fc83c946e9502e3cff191435b11
Andres Toala <andrestoala2013@gmail.com>
2023-02-27 21:18:14 -0500

    fixing bugs with routing of home

--- frontend/src/pages/Home/HomeActions.jsx
+++ frontend/src/pages/Home/HomeActions.jsx
@@ -4,30 +4,40 @@ import {
 	Text,
 	BackgroundImage
 } from '@mantine/core';
+import { Link } from 'react-router-dom';
 
 const HomeActions = () => {
 	return (
 		<>
 			<Grid className='actions'>
 				<Grid.Col className='action' span={3}>
-					<BackgroundImage component='a' href='/especies'  className="image" src="https://upload.wikimedia.org/wikipedia/commons/1/1d/Guaiacum_officinale_-_Köhler–s_Medizinal-Pflanzen-069.jpg">
-						<Text className='action_text'>Especies Nativas</Text>
-					</BackgroundImage>
+					<Link to="/especies" className='linked'>
+						<BackgroundImage component='a' className="image" src="https://upload.wikimedia.org/wikipedia/commons/1/1d/Guaiacum_officinale_-_Köhler–s_Medizinal-Pflanzen-069.jpg">
+							<Text className='action_text'>Especies Nativas</Text>
+						</BackgroundImage>
+					</Link>
+				</Grid.Col>
+				
+				<Grid.Col className='action' span={3}>
+					<Link to="/especies" className='linked'>
+						<BackgroundImage component='a' className="image" src="">
+							<Text className='action_text'>Selector para Sembrío</Text>
+						</BackgroundImage>
+					</Link>
 				</Grid.Col>
 				<Grid.Col className='action' span={3}>
-				<BackgroundImage component='a' href='/especies'  className="image" src="">
-						<Text className='action_text'>Selector para Sembrío</Text>
-					</BackgroundImage>
+					<Link to="/viveros" className='linked'>
+						<BackgroundImage component='a' className="image" src="">
+							<Text className='action_text'>Viveros</Text>
+						</BackgroundImage>
+					</Link>
 				</Grid.Col>
 				<Grid.Col className='action' span={3}>
-				<BackgroundImage component='a' href='/viveros'  className="image" src="">
-						<Text className='action_text'>Viveros</Text>
-					</BackgroundImage>
-				</Grid.Col>
-				<Grid.Col className='action' span={3}>
-				<BackgroundImage component='a' href='/guias'  className="image" src="">
-						<Text className='action_text'>Guías y Consejos</Text>
-					</BackgroundImage>
+					<Link to="/guias" className='linked'>
+						<BackgroundImage component='a' className="image" src="">
+							<Text className='action_text'>Guías y Consejos</Text>
+						</BackgroundImage>
+					</Link>
 				</Grid.Col>
 			</Grid>
 		</>
