3906f0675468858cdabd78b7f4a8342dce20e350
Andres Toala <andrestoala2013@gmail.com>
2023-02-27 21:39:07 -0500

    fixed finally fs

--- frontend/src/pages/Home/HomeActions.jsx
+++ frontend/src/pages/Home/HomeActions.jsx
@@ -11,22 +11,22 @@ const HomeActions = () => {
 		<>
 			<Grid className='actions'>
 				<Grid.Col className='action' span={3}>
-					<BackgroundImage component='a' href="especies"  className="image" src="https://upload.wikimedia.org/wikipedia/commons/1/1d/Guaiacum_officinale_-_Köhler–s_Medizinal-Pflanzen-069.jpg">
+					<BackgroundImage className="image" src="https://upload.wikimedia.org/wikipedia/commons/1/1d/Guaiacum_officinale_-_Köhler–s_Medizinal-Pflanzen-069.jpg">
 						<Text className='action_text' component={Link} to="especies">Especies Nativas</Text>
 					</BackgroundImage>
 				</Grid.Col>
 				<Grid.Col className='action' span={3}>
-				<BackgroundImage component='a' href='especies'  className="image" src="">
+				<BackgroundImage className="image" src="">
 						<Text className='action_text' component={Link} to="especies">Selector para Sembrío</Text>
 					</BackgroundImage>
 				</Grid.Col>
 				<Grid.Col className='action' span={3}>
-				<BackgroundImage component='a' href='viveros'  className="image" src="">
+				<BackgroundImage className="image" src="">
 						<Text className='action_text' component={Link} to="viveros">Viveros</Text>
 					</BackgroundImage>
 				</Grid.Col>
 				<Grid.Col className='action' span={3}>
-				<BackgroundImage component='a' href='guias'  className="image" src="">
+				<BackgroundImage className="image" src="">
 						<Text className='action_text' component={Link} to="guias">Guías y Consejos</Text>
 					</BackgroundImage>
 				</Grid.Col>
