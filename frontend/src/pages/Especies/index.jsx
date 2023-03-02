597fa08a93bad36c4d0faa7b4fd808f7c63551a2
Andres Toala <andrestoala2013@gmail.com>
2023-03-02 00:45:04 -0500

    Finalizado

--- frontend/src/pages/Especies/index.jsx
+++ frontend/src/pages/Especies/index.jsx
@@ -13,6 +13,7 @@ import {
 	Chip,
 	Image,
 	createStyles,
+	Divider,
 } from '@mantine/core';
 import { useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
@@ -66,15 +67,315 @@ const EspeciesPage = () => {
 	};
 	const advanceFilterModal = () => {
 		openModal({
+			size: '80%',
+			centered: true,
 			children: (
 				<Stack className='modal-content'>
 					<Title className="modal-title">Filtro Avanzado</Title>
 					<Text className='modal-text'>Selecciona los filtros que deseas aplicar</Text>
-					<Grid className='filter-box' spacing='md'>
-						<Select placeholder='Altura máxima' data={['menor a 6 m', 'entre 6 m y 16 m', 'mayor a 16 m']}></Select>
-						<Select placeholder='Altura máxima' data={['menor a 6 m', 'entre 6 m y 16 m', 'mayor a 16 m']}></Select>
-						<Select placeholder='Altura máxima' data={['menor a 6 m', 'entre 6 m y 16 m', 'mayor a 16 m']}></Select>
-					</Grid>
+					<Divider sx={{width: '100%'}}/>
+					<Stack w={"100%"}>
+						<Title order={4}>Caracteristicas Generales</Title>
+						<Grid className='modal-grid'>
+							<Select label="Habito de crecimiento" placeholder='Escoja una opción' data={[
+								'Herbácea',
+								'Arbustiva',
+								'Arbórea',
+								'Cactus',
+								'Trepadora',
+								'Otros',
+							]}></Select>
+							<Select label="Estado de conservación" placeholder='Escoja una opción' data={[
+								'Extinta',
+								'Extinta en estado salvaje',
+								'En peligro critico',
+								'En peligro',
+								'Vulnerable',
+								'Casi amenazada',
+								'Preocupación menor',
+								'Datos insuficientes',
+								'No evaluada',
+							]}></Select>
+							<Select label="Altura máxima" placeholder='Escoja una opción' data={['menor a 6 m', 'entre 6 m y 16 m', 'mayor a 16 m']}></Select>
+							<Select label="Limitaciones flores" placeholder='Escoja una opción' data={[
+								'Alergenicas',
+								'Olor desagradable',
+								'Toxicas',
+								'Carnosas',
+								'Masivas',
+								'Grandes',
+								'Ninguna',
+								'No determinado',
+							]}></Select>
+							<Select label="Limitaciones frutos" placeholder='Escoja una opción' data={[
+								'Alergenicas',
+								'Con espinas',
+								'Toxicos',
+								'Carnosos',
+								'Masivos',
+								'Pesados',
+								'Ninguna',
+								'No determinado',
+							]}></Select>
+							<Select label="Uso en espacio publico" placeholder='Escoja una opción' data={[
+								'Cerros',
+								'Parques',
+								'Jardines',
+								'Plazas',
+								'Redondeles',
+								'Edificios publicos',
+								'No determinado',
+								'Vias peatonales',
+								'Otros',
+							]}></Select>
+							<Select label="Longevidad" placeholder='Escoja una opción' data={[
+								'Perenne',
+								'Anual',
+								'Alta',
+								'Media',
+								'Baja',
+								'No determinado',
+							]}></Select>
+							<Select label="Atracción fauna" placeholder='Escoja una opción' data={[
+								'Baja',
+								'Media',
+								'Alta',
+								'No determinado',
+							]}></Select>
+							<Select label="Requerimiento de luminosidad" placeholder='Escoja una opción' data={[
+								'Sombra',
+								'Sombra parcial',
+								'Luz solar',
+								'Luz solar directa',
+								'No determinado',
+							]}></Select>
+							<Select label="Amplitud de copa" placeholder='Escoja una opción' data={[
+								'Nula',
+								'Pequeña',
+								'Media',
+								'Grande',
+								'No determinado',
+							]}></Select>
+							<Select label="Forma de copa" placeholder='Escoja una opción' data={[
+								'Aparasolada',
+								'Columnar',
+								'Estratificada',
+								'Globosa',
+								'Irregular',
+								'Oval',
+								'Pendula',
+								'Piramidal',
+								'Semiglobosa',
+								'Otra',
+								'No determinado',
+							]}></Select>
+							<Select label="Densidad del follaje" placeholder='Escoja una opción' data={[
+								'Baja',
+								'Media',
+								'Alta',
+								'No determinado',
+							]}></Select>
+							<Select label="Zona humedad" placeholder='Escoja una opción' data={[
+								'Seca',
+								'Humeda',
+								'Muy humeda',
+								'No determinado',
+							]}></Select>
+						</Grid>
+						<Divider sx={{width: '100%'}}/>
+						<Title order={4}>Tallo</Title>
+						<Grid className='modal-grid'>
+							<Select label="Atributos de corteza" placeholder='Escoja una opción' data={[
+								'lisa', 
+								'espinas', 
+								'anillos',
+								'estrias', 
+								"fisurada",
+								'otro',
+								'no determinado', 
+							]}></Select>
+							<Select label="Color de corteza" placeholder='Escoja una opción' data={[
+								'Blanco',
+								'Verdoso',
+								'Gris',
+								'Otro',
+								'No determinado'
+							]}></Select>							
+						</Grid>
+						<Divider sx={{width: '100%'}}/>
+						<Title order={4}>Raiz</Title>
+						<Grid className='modal-grid'>
+							<Select label="Forma de reproducción" placeholder='Escoja una opción' data={[
+								'Semillas',
+								'Por estaca',
+								'Por bulbo',
+								'Por tuberculo',
+								'Por rizoma',
+								'Division de pie',
+								'Por estolon',
+								'Por acodo',
+								'Otro',
+								'No determinado', 
+							]}></Select>
+							<Select label="Color de corteza" placeholder='Escoja una opción' data={[
+								'Blanco',
+								'Verdoso',
+								'Gris',
+								'Otro',
+								'No determinado'
+							]}></Select>
+							<Select label="Forma de raiz" placeholder='Escoja una opción' data={[
+								'Radicular',
+								'Fibrosa',
+								'Glandular',
+								'No determinado',
+							]}></Select>
+							<Select label="Tipo de enraizamiento" placeholder='Escoja una opción' data={[
+								'Profundo',
+								'Medio',
+								'Superficial',
+								'No determinado',
+							]}></Select>
+						</Grid>
+						<Divider sx={{width: '100%'}}/>
+						<Title order={4}>Flor</Title>
+						<Grid className='modal-grid'>
+							<Select label="Color de la flor" placeholder='Escoja una opción' data={[
+								'Blanco',
+								'Amarillo',
+								'Rojo',
+								'Otro',
+							]}></Select>
+							<Select label="Disposicion de la flor" placeholder='Escoja una opción' data={[
+								'Solitaria',
+								'Espiga',
+								'Racimo',
+								'Panicula',
+								'Corimbo',
+								'Amento',
+								'Otro',
+								'No determinado',
+							]}></Select>
+							<Select label="Sistema de polinizacion" placeholder='Escoja una opción' data={[
+								'Viento',
+								'Agua',
+								'Insectos',
+								'Aves',
+								'Mamiferos',
+								'Aves nectarivoras',
+								'Otros animales',
+								'No determinado',
+							]}></Select>
+							<Select label="Estacion de floracion" placeholder='Escoja una opción' data={[
+								'Permanente',
+								'Estacional',
+								'Estacion seca',
+								'Estacion lluviosa',
+								'No determinado',
+								'No aplica',
+							]}></Select>
+							<Select label="Meses de floracion" placeholder='Escoja una opción' data={[
+								'Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'
+							]}></Select>
+						</Grid>
+						<Divider sx={{width: '100%'}}/>
+						<Title order={4}>Hoja</Title>
+						<Grid className='modal-grid'>
+							<Select label="Forma de hoja" placeholder='Escoja una opción' data={[
+								'Lineal',
+								'Lineal-lanceolada',
+								'Lanceolada',
+								'Ovada',
+								'Elliptica',
+								'Elipsoide',
+								'Oblonga',
+								'Otro',
+								'No determinado',
+							]}></Select>
+							<Select label="Persistencia de hoja" placeholder='Escoja una opción' data={[
+								'Perenne',
+								'Caducifolia',
+								'Semicaducifolia',
+								'No determinado',
+								'No aplica',
+							]}></Select>
+							<Select label="Posicion de hoja en tallo" placeholder='Escoja una opción' data={[
+								'Alterna',
+								'Opuesta',
+								'Fasciculada',
+								'Roseta',
+								'Verticilada',
+								'No determinado',
+							]}></Select>
+							<Select label="Composicion de hoja" placeholder='Escoja una opción' data={[
+								'Simple',
+								'Digitado Compuesta',
+								'Compuesta paripinnada',
+								'Compuesta imparipinnada',
+								'Compuesta bifoliada',
+								'Compuesta trifoliada',
+								'No determinado',
+							]}></Select>	
+						</Grid>
+						<Divider sx={{width: '100%'}}/>
+						<Title order={4}>Fruto / Semilla</Title>
+						<Grid className='modal-grid'>
+							<Select label="Forma de fruto" placeholder='Escoja una opción' data={[
+								'Capsula',
+								'Legumbre',
+								'Foliculo',
+								'Samara',
+								'Baya',
+								'Otro',
+								'No aplica',
+								'No determinado',
+							]}></Select>
+							<Select label="Color de fruto" placeholder='Escoja una opción' data={[
+								'Blanco',
+								'Rojo',
+								'Verde',
+								'Otro',
+								'No determinado',
+								'No aplica',
+							]}></Select>
+							<Select label="Sistema de dispersión de frutos" placeholder='Escoja una opción' data={[
+								'Viento',
+								'Agua',
+								'Gravedad',
+								'Insectos',
+								'Aves',
+								'Mamiferos',
+								'Aves nectarivoras',
+								'Otros animales',
+								'No determinado',
+								'No aplica',
+							]}></Select>
+							<Select label="Meses de fructificacion" placeholder='Escoja una opción' data={[
+								'Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'
+							]}></Select>
+						</Grid>
+						<Divider sx={{width: '100%'}}/>
+						<Title order={4}>Ecologia / Etnobotanica</Title>
+						<Grid className='modal-grid'>
+							<Select label="Rango altitudinal" placeholder='Escoja una opción' data={[
+								'0 - 1000 msnm',
+								'1000 - 2000 msnm',
+								'2000 - 3000 msnm',
+								'> 3000 msnm',
+								'No determinado',
+							]}></Select>
+							<Select label="Categoria de uso" placeholder='Escoja una opción' data={[
+								'Alimenticia',
+								'Artesanal',
+								'Medicinal',
+								'Ornamental',
+								'Otros',
+								'No determinado',
+							]}></Select>
+						</Grid>
+					</Stack>
+					
+					
 					<Group className='btn-group centered'>
 						<Button color='dark'>Limpiar</Button>
 						<Button onClick={notificationSearch}>Filtrar</Button>
